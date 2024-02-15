3f9c1dc547ba74757dc1551a9d9757f5f0a009a4
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-02-15 14:10:36 -0800

    O(nlogn) 28ms

---
+++ arrays/PolygonPerimeter2971.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long largestPerimeter(int[] nums) {
+        //Sort array
+        Arrays.sort(nums);
+
+        //Sum + Best case
+        long perimeter = -1;
+        long sum = 0;
+
+        for(int i=0; i< nums.length; i++){
+            if( i > 1 && sum > nums[i]){
+                perimeter  = sum + nums[i];
+            }
+
+            sum += nums[i];
+        }
+
+         return perimeter;
+    }
+}
