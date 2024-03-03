64511806cdc40831e7a2da60dd1e353c2af63c6c
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-03-03 15:28:00 -0800

    BFS -> All Path

---
+++ DSA/Graphs/Algos/BFS/AllPath797.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        /*Theory:
+        1. DS: Que, Lists<lists>, 
+        2. So it's like this:
+        3. In every iteration of Que I should should store lists and check if we found the target 
+        4. Else add it back to the list with current elements neighbors
+        5. Eventually hoping the list will return atleast one path if the target exists */
+        int len = graph.length;
+        
+        List<List<Integer>> paths = new ArrayList<>();
+        
+        Queue<List<Integer>> que = new LinkedList<>();
+        que.add(new ArrayList<>(Arrays.asList(0)));
+        
+        while(!que.isEmpty()){
+            List<Integer> path = que.remove();
+            
+            //Take the last element
+            int size = path.size() - 1;
+            int curr = path.get(size);
+            
+            if(curr == len-1){
+                paths.add(new ArrayList<>(path));
+            }
+            
+            //Iterate over all the neighbor of current node and 
+            //Create a list in each iteration and add them to the que
+            for(int i: graph[curr]){
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(i);
+                que.add(newPath);
+            }
+        }
+        
+        return paths;
+    }
+}
