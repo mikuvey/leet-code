6bab9c1ae883639e1b0f015e8dcb7b0a66696a97
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-06-29 21:10:48 -0700

    commit

--- bitwise/singleNumber.py
+++ bitwise/singleNumber.py
@@ -6,4 +6,9 @@ class Solution:
             a = a ^ i
         
         return a
+
+#link: https://www.youtube.com/watch?v=qMPX1AOa83k
+# THe main idea of this solution is that by performing XOR operation on each
+#element of the array we are going to end up on that unique element as the remainig
+#elemts are going be cancelled out eventually as we iterate through the array
         
