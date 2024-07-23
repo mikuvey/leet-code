fc7c5245fcfeefbdd382d977507d509577cedd33
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-07-23 15:48:06 -0700

    Rev Polish problem solved

--- arrays/operators/reverse-polish-operators.py
+++ arrays/operators/reverse-polish-operators.py
@@ -1,29 +1,39 @@
+from collections import deque
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         #Algorithm
         """
-        Wrong Implementation: Iterate over the list, when op is found -> Get last two elements and do the operation and insert the result back in the op index; Perform the same for entire the array/list
+        Iterate over the list, when op is found -> Get last two elements and do the operation and insert the result back in the op index; Perform the same for entire the array/list
         """
-        #DS required -> Nothing; I believe we could manage it using a switch cases
-        for i in range(len(tokens)):
-            if tokens[i] == "+":
-                tokens[i] = tokens[i-2] + tokens[i - 1]
-                tokens.pop(i-2)
-                tokens.pop(i-1)
-            elif tokens[i] == "-":
-                tokens[i] = tokens[i-2] - tokens[i - 1]
-                tokens.pop(i-2)
-                tokens.pop(i-1)
-            elif tokens[i] == "/":
-                tokens[i] = int(tokens[i-2] / tokens[i - 1])
-                tokens.pop(i-2)
-                tokens.pop(i-1)
-            elif tokens[i] == "*":
-                tokens[i] = tokens[i-2] * tokens[i - 1]
-                tokens.pop(i-2)
-                tokens.pop(i-1)
-            else:
-                continue
+        #DS -> Using the stack is the most intutive way of solving this problem
         
-        return tokens[0]
+        stack = deque()
+        result = int(tokens[0])
+        
+        for i in range(len(tokens)):
             
+            if tokens[i] == "+":
+                b = stack.pop()
+                a = stack.pop()
+                result = int(a) + int(b)
+                stack.append(result)
+            elif tokens[i] == "-":
+                b = stack.pop()
+                a = stack.pop()
+                result = int(a) - int(b)
+                stack.append(result)
+            elif tokens[i] == "*":
+                b = stack.pop()
+                a = stack.pop()
+                result = int(a) * int(b)
+                stack.append(result)
+            elif tokens[i] == "/":
+                b = stack.pop()
+                a = stack.pop()
+                result = int(int(a)/int(b))
+                stack.append(result)
+            else:
+                stack.append(tokens[i])
+        
+        return result
