7f618a9ee0a8fa22e7da313a6c491d9e266dd165
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-08-01 19:32:26 -0700

    StringAlmostEq Better approach

--- arrays/StringAlmostEq*.py
+++ arrays/StringAlmostEq*.py
@@ -1,44 +1,15 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
-        #{a:1, b: 1., c:1, d:1, e:2, f:1}, {a:4, b:1,c:2}
-        dic1 = {}
-        dic2 = {}
+        counter = [0] * 26
 
+        #Itereate with len(word1) as length is same for both
         for i in range(len(word1)):
-            s = word1[i]
-            if s in dic1:
-                dic1[s] += 1
-            else:
-                dic1[s] = 1
+            # Increment if character i found in word1 and viceversa
+            counter[ord(word1[i]) - ord('a')] += 1
+            counter[ord(word2[i]) - ord('a')] -= 1
         
-        for i in range(len(word2)):
-            s = word2[i]
-            if s in dic2:
-                dic2[s] += 1
-            else:
-                dic2[s] = 1
-        
-        #Compare maps
-        #Challenge (comparing non existant keys) -> Could be solved with condition
-        #Challenge is looping through which dictionary (based on max length? -> Results in redundant code)
-
-        #Two pass
-        for i in dic1.keys():
-            if i in dic2:
-                #seen.append(i) //add(i)?
-                sub = abs(dic1[i] - dic2[i])
-                if sub > 3:
-                    return False
-            else:
-                if dic1[i] > 3:
-                    return False
-
-        for i in dic2.keys():
-            if i in dic1:
-                continue
-            else:
-                if dic2[i] > 3:
-                    return False
+        #Iterate to check if any element is greater than 3
+        for i in range(len(counter)):
+            if abs(counter[i]) > 3:
+                return False
         return True
-            
-        
