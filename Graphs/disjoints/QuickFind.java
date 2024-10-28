b4a38f6f8de7b5d6f0280cf4a0b68a44fa1dfa94
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-10-28 06:17:10 -0700

    QuickFind Algoritm

--- Graphs/disjoints/QuickFind.java
+++ Graphs/disjoints/QuickFind.java
@@ -1,14 +1,40 @@
 package Graphs.disjoints;
 
 public class QuickFind {
-    public statc void main(String[] args){
-        int[][] input = new int[][];
-        
-        QuickFind qf = new QuickFind();
+    
+    private int[] disjointSet;
+
+    public QuickFind(int size){
+        //Build a disjoint set focusing and find operation O(1)
+        disjointSet = new int[size];
+
+        //Initially every element is a root of itself
+        for(int i=0; i<size; i++){
+            disjointSet[i] = i;
+        }
     }
 
-    public QuickFind(int[][] input){
-        //Build a disjoint set focusing and find operation O(1)
+    public void union(int x, int y){
+        //If already connected exist
+        rootX = disjointSet[x];
+        rootY = disjointSet[y];
+
+        if(rootX != rootY){
+            for(int i=0; i<disjointSet.length; i++{
+                if(disjointSet[i] == rootY){
+                    disjointSet[i] = rootX;
+                }
+            }
+        }
+    }
+
+    public boolean connected(int x, int y){
+        if(disjointSet[x] == disjointSet[y]) return true;
+        return false;
+    }
+    public statc void main(String[] args){
+        //Given a graph -> Transform it into disjontset using Union method then we are all set
+        QuickFind qf = new QuickFind();
         
     }
 }
